b4599f12bdba460a0f0bc1df23e2eafa75d61fe8
mlewissmith <mlewissmith@users.noreply.github.com>
2026-03-09 12:32:20 +0000

    [GH-19] rpmlsf - remove all options
    We don't need them; rpmlsf by definition should display full details

--- rpmlsf.bash
+++ rpmlsf.bash
@@ -1,20 +1,15 @@
 #!/bin/bash
 set -u
 
-all=
-owner=
-
 function _usage {
     cat <<-EOF
 	rpmlsf - List contents of rpm packages
 
 	Usage:
-	    rpmlsf [-a] [-l] PKG...
+	    rpmlsf PKG...
 	    rpmlsf -h
 
 	Options:
-	   -a  list all packages matching a PKG
-	   -l  use a long listing format
 	   -h  print this help and exit
 
 	The PKG arguments may either be package name expressions or file names.
@@ -27,24 +22,17 @@ function _usage {
 while getopts 'alh' opt
 do
     case $opt in
-        a) all=a ;;
-        l) owner='%-8{fileusername} %-8{filegroupname} ' ;;
         h) _usage ;;
     esac
 done
 shift $((OPTIND - 1))
 [[ -z "$@" ]] && _usage
 
-qf="[%-4{fileflags:fflags} %-11{filemodes:perms} $owner%{filenames}\\n]"
-
 for arg in "$@"
 do
-    a=$all
     p=
-    if [[ -f "$arg" ]]
-    then
-        a=
-        p=p
-    fi
-    rpm -q$a$p --qf="$qf" --nodigest --nosignature "$arg"
+    [[ -f "$arg" ]] && p=p
+    rpm -q$p \
+        --queryformat="[%-2{fileflags:fflags} %-11{filemodes:perms} %-6{fileusername} %-6{filegroupname} %{filenames}\n]" \
+        --nodigest --nosignature "$arg"
 done
